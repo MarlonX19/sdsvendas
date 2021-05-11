c87c0b9c8c1d09e09313ea41bfba36c8975be7f3
marlonx19 <marlon.englemam@gmail.com>
2021-05-11 00:51:41 -0300

    first request

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Chart from 'react-apexcharts';
+import axios from 'axios';
+import { BASE_URL } from 'utils/requests';
+
+import { SaleSum } from 'types/sale';
+
+
+type ChartData = {
+  labels: string[];
+  series: number[];
+}
 
 
 const DonutChart: React.FC = () => {
+
+  let chartData: ChartData = { labels: [], series: [] };
+
+  const fetchAmountBySeller = async() => {
+    try {
+      const response = await axios.get(`${BASE_URL}/sales/amount-by-seller`);
+      const data = response.data as SaleSum[];
+      const myLabels = data.map(x => x.sellerName);
+      const mySeries = data.map(x => x.sum);
+
+      chartData = { labels: myLabels, series: mySeries };
+
+      console.log('response', chartData);
+    }
+    catch (err) {
+      console.log('err', err);
+    }
+  }
+
+  useEffect(() => {
+    fetchAmountBySeller();
+  }, [])
+
 
   const mockData = {
     series: [477138, 499928, 444867, 220426, 473088],
@@ -18,8 +51,8 @@ const options = {
   return (
     <div>
       <Chart 
-      options={{...options, labels: mockData.labels}} 
-      series={mockData.series}
+      options={{...options, labels: chartData.labels}} 
+      series={chartData.series}
       type='donut'
       height='240'
     />
---
+++ frontend/src/utils/requests.ts
@@ -0,0 +1,4 @@
+import axios from 'axios';
+
+
+export const BASE_URL = 'https://sdsvendas-x19.herokuapp.com';
